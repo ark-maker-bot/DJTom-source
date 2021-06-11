d851936b5faf211db14aabe6dcf778e588ea3baf
ark <67444302+ark-maker-bot@users.noreply.github.com>
2021-06-11 21:30:49 +0100

    Delete servers.js

--- cmds/servers.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'servers',
-  aliases: ['svs'],
-  description: 'Shows the amount of servers that the bot is in',
-  usage: 'servers',
-  async run(client, msg, args){
-    msg.channel.send({ embed: { description: `I'm in ${msg.client.guilds.cache.size} rehab centres` } });
-  }
-}
