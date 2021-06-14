05afb29c24c4552446ecd52e5f2e3e4824130fb2
ark <67444302+ark-maker-bot@users.noreply.github.com>
2021-06-14 20:22:10 +0100

    added spotify music capability

--- cmds/play.js
+++ cmds/play.js
@@ -1,5 +1,6 @@
 const yt = require('ytdl-core'), fs = require('fs'), ply = require('ytpl'), sr = require('ytsr'), { MessageEmbed } = require('discord.js'), reply = require('../other/reply');
 const { isArray } = require('util');
+const { getData, getTracks, getPreview } = require('spotify-url-info');
 
 module.exports = {
   name: 'play',
@@ -64,35 +65,32 @@ async function seek_track(song, msg, sec){
   const max_seconds = Math.floor(queue.songs[0].duration), s = parseInt(sec);
   if(s >= max_seconds)return msg.reply({ embed: { description: `The entered value is larger than the song's duration` } });
   if(!msg.guild.me.voice.serverDeaf)msg.guild.me.voice.setDeaf(true);
-	queue.songs[0].seek = s;
+  queue.songs[0].seek = s;
   const dispatcher = queue.connection
-  .play(yt(song, {  'quality': 'highest', 'filter': 'audioonly' }), { seek: s })
+  .play(yt(song, {  'quality': 'highest', 'filter': 'audioonly', seek: s }))
   .on('finish', async () => {
+    if(!check_queue(msg)){ msg.client.queue.delete(msg.guild.id); return; }
     msg.client.prev_song = queue.songs[0];
     if(queue.replay){ queue.replay = false; }
     else { if(!queue.loop)queue.songs.shift(); }
     if(!queue.songs[0] || queue.songs[0] === undefined){
       const song_wait = setInterval(async() => {
-        let q = msg.client.queue.get(msg.guild.id);
-        if(!q || !q.songs) { clearInterval(song_wait); }
-        let songs = q.songs;
+        if(!check_queue(msg)){ msg.client.queue.delete(msg.guild.id); clearInterval(song_wait); }
+        let q = msg.client.queue.get(msg.guild.id), songs = q.songs;
         if(songs[0] !== undefined){ await start_track(songs[0].url, msg); clearInterval(song_wait); }
-        // else clearInterval(song_wait);
-      }, 1000);
+      }, 2000);
     }else{ update_ids(msg, queue.songs); await start_track(queue.songs[0].url, msg); }
   })
   .on('error', async err => {
-    // msg.client.prev_song = queue.songs[0];
+    if(!check_queue(msg)){ msg.client.queue.delete(msg.guild.id); return; }
     if(queue.replay){ queue.replay = false; }
     else { if(!queue.loop)queue.songs.shift(); }
     if(!queue.songs[0] || queue.songs[0] === undefined){
       const song_wait = setInterval(async() => {
-        let q = msg.client.queue.get(msg.guild.id);
-        if(!q || !q.songs) { q.voiceChannel.leave(); clearInterval(song_wait); }
-        let songs = q.songs;
+        if(!check_queue(msg)){ msg.client.queue.delete(msg.guild.id); clearInterval(song_wait); }
+        let q = msg.client.queue.get(msg.guild.id), songs = q.songs;
         if(songs[0] !== undefined){ await start_track(songs[0].url, msg); clearInterval(song_wait); }
-        // else clearInterval(song_wait);
-      }, 1000);
+      }, 2000);
     }else{ update_ids(msg, queue.songs); await start_track(queue.songs[0].url, msg); }
   });
   dispatcher.setVolume((queue.volume / 100));
@@ -108,31 +106,28 @@ async function start_track(song, msg){
   const dispatcher = queue.connection
   .play(yt(song, {  'quality': 'highest', 'filter': 'audioonly'  }))
   .on('finish', async () => {
+    if(!check_queue(msg)){ msg.client.queue.delete(msg.guild.id); return; }
     msg.client.prev_song = queue.songs[0];
     if(queue.replay){ queue.replay = false; }
     else { if(!queue.loop)queue.songs.shift(); }
     if(!queue.songs[0] || queue.songs[0] === undefined){
       const song_wait = setInterval(async() => {
-        let q = msg.client.queue.get(msg.guild.id);
-        if(!q || !q.songs) { clearInterval(song_wait); }
-        let songs = q.songs;
+        if(!check_queue(msg)){ msg.client.queue.delete(msg.guild.id); clearInterval(song_wait); }
+        let q = msg.client.queue.get(msg.guild.id), songs = q.songs;
         if(songs[0] !== undefined){ await start_track(songs[0].url, msg); clearInterval(song_wait); }
-        // else clearInterval(song_wait);
-      }, 1000);
+      }, 2000);
     }else{ update_ids(msg, queue.songs); await start_track(queue.songs[0].url, msg); }
   })
   .on('error', async err => {
-    // msg.client.prev_song = queue.songs[0];
+    if(!check_queue(msg)){ msg.client.queue.delete(msg.guild.id); return; }
     if(queue.replay){ queue.replay = false; }
     else { if(!queue.loop)queue.songs.shift(); }
     if(!queue.songs[0] || queue.songs[0] === undefined){
       const song_wait = setInterval(async() => {
-        let q = msg.client.queue.get(msg.guild.id);
-        if(!q || !q.songs) { q.voiceChannel.leave(); clearInterval(song_wait); }
-        let songs = q.songs;
+        if(!check_queue(msg)){ msg.client.queue.delete(msg.guild.id); clearInterval(song_wait); }
+        let q = msg.client.queue.get(msg.guild.id), songs = q.songs;
         if(songs[0] !== undefined){ await start_track(songs[0].url, msg); clearInterval(song_wait); }
-        // else clearInterval(song_wait);
-      }, 1000);
+      }, 2000);
     }else{ update_ids(msg, queue.songs); await start_track(queue.songs[0].url, msg); }
   });
   dispatcher.setVolume((queue.volume / 100));
@@ -149,13 +144,21 @@ function update_ids(msg, songs){
   let q = msg.client.queue.get(msg.guild.id);
   q.songs = songs;
 }
+function check_queue(msg){
+  let queue = msg.client.queue.get(msg.guild.id);
+  if(!queue || queue === [] || !queue.voiceChannel || !queue.textChannel)return false;
+  return true;
+}
 async function managing_data(client, msg, input){
   if(!input)return null;
-  let is_youtube_link = false;
+  let is_youtube_link = false, is_spotify_link = false;
   if(input.includes('youtube.com') || input.includes('youtu.be'))is_youtube_link = true;
+  if(input.includes('open.spotify.com'))is_spotify_link = true;
   const server_queue = client.queue.get(msg.guild.id);
   if(is_youtube_link){
     if(input.includes('list')){
+      const is_valid = yt.validateURL(input);
+      if(is_valid)return null;
       const id_of_playlist = input.split('list')[1].replace('=', ''), playlist = await ply(id_of_playlist, { limit: 20 });
       if(!playlist || !playlist.items || playlist.visibility !== 'everyone')return null;
       const songs = [];
@@ -169,6 +172,8 @@ async function managing_data(client, msg, input){
       }
       return songs;
     }else{
+      const is_valid = yt.validateURL(input);
+      if(is_valid)return null;
       const basic_info = await yt.getBasicInfo(input);
       if(!basic_info)return null;
       const title = basic_info.videoDetails.title, duration = basic_info.videoDetails.lengthSeconds, view_count = basic_info.videoDetails.viewCount; // audio = yt(input, { 'quality': 'highest', 'filter': 'audioonly' }).pipe(fs.createWriteStream(`./songs/${title.split(/ +/).join('_')}.mp3`));
@@ -182,6 +187,25 @@ async function managing_data(client, msg, input){
       }
       return return_data;
     }
+  }else if(is_spotify_link){
+    const data = await getPreview(input);
+    if(!data)return null;
+    const search_results = await sr(`${data.artist} - ${data.title}`, {pages: 1, limit: 2});
+    if(!search_results)return null;
+    const id = server_queue ? server_queue.songs.length : 0, url = search_results.items[0].url, is_valid = yt.validateURL(url);
+    if(!is_valid)return null;
+    const basic_info = await yt.getBasicInfo(search_results.items[0].url); 
+    if(!basic_info)return null; 
+    const title = basic_info.videoDetails.title, duration = basic_info.videoDetails.lengthSeconds, view_count = basic_info.videoDetails.viewCount; // audio = yt(url, { 'quality': 'highest', 'filter': 'audioonly' }).pipe(fs.createWriteStream(`./songs/${title.split(/ +/).join('_')}.mp3`));
+    const return_data = {
+      title: title,
+      view_count: view_count,
+      duration: duration,
+      url: url,
+      seek: 0,
+      id: id
+    }
+    return return_data;
   }else{
     if(!input)return null;
     const search_results = await sr(input, {pages: 1, limit: 2});
